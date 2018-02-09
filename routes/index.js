b04bb7b4b3e87a4a36654c86ed527d5bfde874a8
Rozman <j_rozman@tss-hr420-dm03.fc.ca>
2018-02-09 11:29:21 -0500

    working with ejs

--- routes/index.js
+++ routes/index.js
@@ -1,12 +1,18 @@
 var express = require('express');
 var router = express.Router();
 
+const templateTypes [
+  { name: "Jade", usefulness : "sucky"},
+  { name: "EJS", usefulness : "a little better"},
+  { name: "Handlebars", usefulness : "awesomesauce"}
+]
 /* GET home page. */
 router.get('/', function(req, res, next) {
   res.render('index', {
-     title: 'Jen Rozman',
-     description: "sucks",
-     method: "easy"
+     title: 'EJS',
+     descriptor: "much better",
+     method: "easy",
+     templates: templateTypes /*var name in the template*/
    });
 });
 
