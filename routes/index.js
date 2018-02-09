2c2f8804e05d2e22b2bbaa442ad4dd16cb7ee3dd
Rozman <j_rozman@tss-hr420-dm03.fc.ca>
2018-02-09 11:47:00 -0500

    adding handlebars

--- routes/index.js
+++ routes/index.js
@@ -1,17 +1,17 @@
 var express = require('express');
 var router = express.Router();
 
-const templateTypes [
+var templateTypes = [
   { name: "Jade", usefulness : "sucky"},
   { name: "EJS", usefulness : "a little better"},
   { name: "Handlebars", usefulness : "awesomesauce"}
 ]
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', {
-     title: 'EJS',
-     descriptor: "much better",
-     method: "easy",
+  res.render('home', {
+     title: 'Handlebars',
+     //descriptor: "much better",
+     //method: "easy",
      templates: templateTypes /*var name in the template*/
    });
 });
